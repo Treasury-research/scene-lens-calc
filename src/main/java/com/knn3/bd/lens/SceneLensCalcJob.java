e19c778d6bef3317a593a0f253456be39492c1ce
yong.zhou <yong.zhou@beantechs.com>
2022-12-23 16:42:03 +0800

    add:添加db sink

--- src/main/java/com/knn3/bd/lens/SceneLensCalcJob.java
+++ src/main/java/com/knn3/bd/lens/SceneLensCalcJob.java
@@ -5,20 +5,20 @@ import com.knn3.bd.lens.func.LensDetailJoinFunction;
 import com.knn3.bd.lens.func.LensDetailUnionFunction;
 import com.knn3.bd.lens.func.LensDupFunction;
 import com.knn3.bd.lens.func.LensMapFunction;
-import com.knn3.bd.lens.model.DataWrapper;
-import com.knn3.bd.lens.model.LensBroadModel;
-import com.knn3.bd.lens.model.LensCollect;
-import com.knn3.bd.lens.model.LensPublication;
+import com.knn3.bd.lens.model.*;
 import com.knn3.bd.lens.source.BroadHistorySource;
 import com.knn3.bd.rt.Job;
 import com.knn3.bd.rt.connector.kafka.KafkaSchema;
 import com.knn3.bd.rt.connector.kafka.SourceModel;
 import com.knn3.bd.rt.model.EnvConf;
+import com.knn3.bd.rt.utils.JDBCUtils;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.state.MapStateDescriptor;
 import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
+import org.apache.flink.connector.jdbc.JdbcExecutionOptions;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
@@ -46,6 +46,14 @@ public class SceneLensCalcJob {
         String lensTopic = paramMap.get(EnvConf.KAFKA_TOPIC_POLYGON_LENS);
         Long startTime = Optional.ofNullable(paramMap.get(EnvConf.START_TIME)).map(Long::parseLong).orElse(null);
 
+        String pgUrl = paramMap.get(EnvConf.PG_URL);
+        String pgUsername = paramMap.get(EnvConf.PG_USERNAME);
+        String pgPassword = paramMap.get(EnvConf.PG_PASSWORD);
+
+        String tidbUrl = paramMap.get(EnvConf.TIDB_URL);
+        String tidbUsername = paramMap.get(EnvConf.TIDB_USERNAME);
+        String tidbPassword = paramMap.get(EnvConf.TIDB_PASSWORD);
+
         int sourceP = Job.limit(p, 3);
 
         /**
@@ -71,12 +79,33 @@ public class SceneLensCalcJob {
         MapStateDescriptor<String, List<LensBroadModel>> broadcastDescriptor = new MapStateDescriptor<>("BroadcastDescriptor", TypeInformation.of(String.class), TypeInformation.of(new TypeHint<List<LensBroadModel>>() {
         }));
 
+        /**
+         * jdbc参数
+         */
+        JdbcExecutionOptions jdbcExecutionOptions = JdbcExecutionOptions.builder()
+                .withBatchSize(5000)
+                .withBatchIntervalMs(20000)
+                .withMaxRetries(5)
+                .build();
+        JdbcConnectionOptions pgJdbcConnectionOptions = new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
+                .withUrl(pgUrl)
+                .withDriverName(JDBCUtils.POSTGRES)
+                .withUsername(pgUsername)
+                .withPassword(pgPassword)
+                .build();
+        JdbcConnectionOptions tidbJdbcConnectionOptions = new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
+                .withUrl(tidbUrl)
+                .withDriverName(JDBCUtils.MYSQL)
+                .withUsername(tidbUsername)
+                .withPassword(tidbPassword)
+                .build();
+
 
         SingleOutputStreamOperator<DataWrapper> dupDs = env.fromSource(lensSource, WatermarkStrategy.noWatermarks(), "LensSource").name("LensSource").uid("LensSource").setParallelism(sourceP)
                 .flatMap(new LensMapFunction()).name("LensMap").uid("LensMap")
                 .keyBy(x -> x.f0)
                 .process(new LensDupFunction(pubTag, collectTag)).name("LensDup").uid("LensDup");
-        dupDs.getSideOutput(collectTag).keyBy((KeySelector<LensCollect, String>) value -> String.join(Cons.SEP, value.getRootProfileId(), value.getRootPubId()))
+        SingleOutputStreamOperator<LensDetail> outDs = dupDs.getSideOutput(collectTag).keyBy((KeySelector<LensCollect, String>) value -> String.join(Cons.SEP, value.getRootProfileId(), value.getRootPubId()))
                 .connect(dupDs.getSideOutput(pubTag).keyBy((KeySelector<LensPublication, String>) value -> String.join(Cons.SEP, value.getProfileId(), value.getPubId())))
                 .process(new LensDetailJoinFunction()).name("Join").uid("Join")
                 .connect(
@@ -84,8 +113,48 @@ public class SceneLensCalcJob {
                                 .union(dupDs)
                                 .broadcast(broadcastDescriptor)
                 )
-                .process(new LensDetailUnionFunction(broadcastDescriptor)).name("BroadUnion").uid("BroadUnion")
-                .print();
+                .process(new LensDetailUnionFunction(broadcastDescriptor)).name("BroadUnion").uid("BroadUnion");
+
+        outDs.print();
+
+        // outDs.addSink(JdbcSink.sink(
+        //         "Cons.SQL_NFT_TOKEN_HOLD_RT",
+        //         (statement, hold) -> {
+        //             String dt = hold.getBlockDate();
+        //             String tokenType = hold.getTable();
+        //             String address = hold.getAddress();
+        //             String contract = hold.getContract();
+        //             BigInteger count = TransService.strToBigInt(hold.getCount());
+        //
+        //             JdbcService.setString(1, statement, dt);
+        //             JdbcService.setString(2, statement, tokenType);
+        //             JdbcService.setString(3, statement, address);
+        //             JdbcService.setString(4, statement, contract);
+        //             JdbcService.setObject(5, statement, count);
+        //             JdbcService.setObject(6, statement, count);
+        //         },
+        //         jdbcExecutionOptions,
+        //         pgJdbcConnectionOptions
+        // ));
+        // outDs.addSink(JdbcSink.sink(
+        //         "Cons.SQL_NFT_TOKEN_HOLD_RT",
+        //         (statement, hold) -> {
+        //             String dt = hold.getBlockDate();
+        //             String tokenType = hold.getTable();
+        //             String address = hold.getAddress();
+        //             String contract = hold.getContract();
+        //             BigInteger count = TransService.strToBigInt(hold.getCount());
+        //
+        //             JdbcService.setString(1, statement, dt);
+        //             JdbcService.setString(2, statement, tokenType);
+        //             JdbcService.setString(3, statement, address);
+        //             JdbcService.setString(4, statement, contract);
+        //             JdbcService.setObject(5, statement, count);
+        //             JdbcService.setObject(6, statement, count);
+        //         },
+        //         jdbcExecutionOptions,
+        //         tidbJdbcConnectionOptions
+        // ));
 
 
         env.execute(jobName);
